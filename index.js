33707c66e62927ab98e8622fbcc738eb88b05c79
Preuß <joshua.preuss2000@gmx.de>
2019-10-11 13:34:41 +0200

    11.10.9

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const webserver = express();
 const classifier = require('./classifier')
+const PORT = process.env.PORT || 8080;
 
 webserver.use(express.urlencoded());
 webserver.get('/', express.static('public'));
 webserver.post('/api/classify', classifier.handle);
 webserver.listen(PORT);
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-webserver.listen(port);
